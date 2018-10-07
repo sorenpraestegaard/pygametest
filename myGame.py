65cd90e331f20323c369018e06bf391008888f31
Søren Præstegaard <soren_praestegaard@hotmail.com>
2018-10-07 20:22:07 +0200

    Fixed the window shutdown procedure

--- myGame.py
+++ myGame.py
@@ -9,11 +9,19 @@ pos = [150, 100]
 black = 0,0,0
 time = 0
 screen = pygame.display.set_mode(size)
+running = True
 
-while True:
+while running:
+	for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+
 	time = time + 0.01
 	pos[0] = 50 * math.sin(time)
 
 	screen.fill(black)
-	pygame.draw.rect(screen, (255, 0, 0), [pos[0], pos[1], 10, 10], 2)
+	pygame.draw.rect(screen, (255, 0, 100), [pos[0], pos[1], 10, 10], 2)
 	pygame.display.flip()
+
+pygame.display.quit()
+pygame.quit()
